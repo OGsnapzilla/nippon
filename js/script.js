548603ac3b9a81e24410ef445b54ef388f2db6cb
Andreaesther <eaa23aesmp@students.eaaa.dk>
2024-10-08 11:58:54 +0200

    show more-show less function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict;"
+
+/*Show more - show less function*/
+function toggleText(tekstId, link) {
+    var tekst = document.getElementById(tekstId);
+
+    if (tekst.classList.contains("full")) {
+        tekst.classList.remove("full");
+        link.innerHTML = "Vis mere";
+    } else {
+        tekst.classList.add("full");
+        link.innerHTML = "Vis mindre";
+    }
+}
