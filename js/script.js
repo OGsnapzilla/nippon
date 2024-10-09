af073cc717d9630d123f319a2fa1461118af0204
Andreaesther <eaa23aesmp@students.eaaa.dk>
2024-10-09 11:53:26 +0200

    scroll to top button

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ function toggleText(tekstId, link) {
         link.innerHTML = "Vis mindre";
     }
 }
+
+
+/*Scroll to top button*/
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollBtn = document.getElementById("scrollBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+}
